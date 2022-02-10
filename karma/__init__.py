add72819fd47df7f1afa92f0bff0f06df35378a7
Jagmohan Dixit <78152773+Jagmohan-Dixit@users.noreply.github.com>
2022-02-10 20:30:08 +0530

    Update __init__.py

--- karma/__init__.py
+++ karma/__init__.py
@@ -52,7 +52,7 @@ app.register_blueprint(additem)
 
 # @app.route('/')
 # def index():
-#   return render_template('index.html')
+#     return render_template('index.html')
 
 @app.route('/contact')
 def contact():
